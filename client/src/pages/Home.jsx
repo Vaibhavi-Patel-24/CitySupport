3ce51e76919da16beb691659db73b166136b755e
Vaibhavi-Patel-24 <121277667+Vaibhavi-Patel-24@users.noreply.github.com>
2025-02-03 22:02:53 +0530

    Banner component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
 
 export default function Home(){
     return(
        <div>
              <Navbar/>
+             <Banner/>
        </div>
     )
 }
