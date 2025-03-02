eaa0978716902a723089ad795da564e603eb28d3
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-03-02 21:01:52 +0530

    FAQs for client has been done

--- client/src/pages/Help.jsx
+++ client/src/pages/Help.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ContactMain from '../components/ContactMain'
+import FAQSection from '../components/FAQs'
 const Help = () => {
   return (
     <div className="page-container">
         <Navbar/>
         <div className="content">
         <ContactMain/>
+        <FAQSection/>
         </div>
         <Footer/>
     </div>
